cab0e7f21564d760d79a4f01f36df70b09847bff
Lufanin <orion.pax37@gmail.com>
2024-09-06 18:33:01 +0300

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,50 @@
+import threading
+import random
+import time
+
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            with self.lock:
+                self.balance += amount
+                if self.balance >= 500 and not self.lock.locked():
+                    self.lock.release()
+                print(f"Пополнение: {amount}. Баланс: {self.balance}")
+            time.sleep(0.001)  # Имитация времени выполнения
+
+    def take(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            print(f"Запрос на {amount}")
+            with self.lock:
+                if amount <= self.balance:
+                    self.balance -= amount
+                    print(f"Снятие: {amount}. Баланс: {self.balance}")
+                else:
+                    print("Запрос отклонён, недостаточно средств")
+                    self.lock.acquire()  # Блокируем поток при недостатке средств
+            time.sleep(0.001)  # Имитация времени выполнения
+
+# Создание объекта класса Bank
+bk = Bank()
+
+# Создание потоков для методов deposit и take
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+# Запуск потоков
+th1.start()
+th2.start()
+
+# Ожидание завершения потоков
+th1.join()
+th2.join()
+
+# Итоговый баланс
+print(f'Итоговый баланс: {bk.balance}')
