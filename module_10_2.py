267a7bba87164a8c5d56b2c3d4f68ff8bf74023d
Lufanin <orion.pax37@gmail.com>
2024-09-06 18:34:22 +0300

    Update module_10_2.py

--- module_10_2.py
+++ module_10_2.py
@@ -14,7 +14,7 @@ class Knight(threading.Thread):
         print(f"{self.name}, на нас напали!")
 
         while self.enemies > 0:
-            time.sleep(1)  # Задержка на 1 секунду
+            time.sleep(1)  
             self.days += 1
             self.enemies -= self.power
 
@@ -26,15 +26,12 @@ class Knight(threading.Thread):
         print(f"{self.name} одержал победу спустя {self.days} дней(дня)!")
 
 
-# Создание рыцарей
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight('Sir Galahad', 20)
 
-# Запуск потоков
 first_knight.start()
 second_knight.start()
 
-# Ожидание завершения потоков
 first_knight.join()
 second_knight.join()
 
